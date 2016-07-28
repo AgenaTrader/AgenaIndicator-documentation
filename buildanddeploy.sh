040ac971a11d349b2046f61d62759163bc5c0611
Simon Pucher <simon.pucher@creativo.at>
2016-07-28 11:35:49 +0200

    new deploy

--- buildanddeploy.sh
+++ buildanddeploy.sh
@@ -41,6 +41,8 @@ then
   git diff-tree -r --name-only --no-commit-id master | grep '^site/' &> /dev/null
   if [ $? == 0 ]; then
     git push origin `git subtree split --prefix site master 2> /dev/null`:gh-pages --force
+    # merge master
+    # git commit -a -m "Automatic deploy: $TODAY"
   fi
 
   exit

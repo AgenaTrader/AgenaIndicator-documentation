0850d8a1c96b514506ba9e9836b5be8877a2fa5c
simonpucher <simon.pucher@creativo.at>
2016-10-04 21:44:12 +0200

    new deploy for gh-pages

--- buildanddeploy.bat
+++ buildanddeploy.bat
@@ -1,2 +1,7 @@
 REM build batch file for windows
-echo todo
+echo *** starting build process ***
+mkdocs build --clean
+echo *** starting deploy process ***
+git subtree split --prefix site master
+git push origin :gh-pages --force
+echo *** success ***

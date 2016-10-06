a7faf7c2e4f7f9083b3887eaa19fde767e1e390e
simonpucher <simon.pucher@creativo.at>
2016-10-06 17:22:46 +0200

    deploy on windows not working

--- buildanddeploy.bat
+++ buildanddeploy.bat
@@ -1,8 +1,8 @@
 REM build batch file for windows
 echo *** starting build process ***
-mkdocs build --clean
+REM mkdocs build --clean
 echo *** starting deploy process ***
-git add --all && git commit -m "new deploy for gh-pages"
-git subtree split --prefix site master
-git push origin :gh-pages --force
+echo git add --all && git commit -m "new deploy for gh-pages"
+REM git subtree split --prefix site master
+REM git push origin :gh-pages --force
 echo *** success ***
